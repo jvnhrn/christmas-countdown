d2227496d346d2437ff2e24b9579baec0c4bd9f5
Johanna Horn <j.von.horn@hotmail.com>
2020-12-10 11:37:03 +0100

    update christmas clac

--- js/script.js
+++ js/script.js
@@ -3,19 +3,29 @@ function realTimeCountdown() {
     // Set up Variables 
     var today = new Date();
     var newToday = today.toDateString();
-    var christmas = new Date(2020,11,24);
+    var christmas = new Date();
+   
+    christmas.setDate(24);
+    christmas.setMonth(11);
+
     // var christmasEve = christmas.toDateString();
 
     console.log(today);
     console.log(christmas);
+    //(1,2,3,..., 31)
+    // var Difference_In_Time = date2.getTime() - date1.getTime();  
+    var diff = (christmas.getTime() - today.getTime());
 
-    var diff = (today.getDate() - christmas.getDate()) * (-1);
+    if (diff < 0){
+        christmas.setFullYear(christmas.getFullYear() + 1);
+        diff = (christmas.getTime() - today.getTime()); 
+    }
 
-    console.log(diff);
+    var diffInDays = parseInt(diff / (1000 * 3600 * 24));
 
 
     //display the clock 
-    document.getElementById('clock').innerHTML = diff + " Days";
+    document.getElementById('clock').innerHTML = diffInDays + " Days";
     document.getElementById('today').innerHTML = "TODAY: " + newToday;
     // document.getElementById('christmas').innerHTML = "CHRITSMAS EVE: " + christmasEve;
     var t = setTimeout(realTimeCountdown, 500);
